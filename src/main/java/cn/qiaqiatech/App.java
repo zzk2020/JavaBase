d983a979fdb1a4b125bba4a8f1f52605c5a2df17
zzk <likaizzk@126.com>
2020-06-01 15:40:46 +0800

    init

---
+++ src/main/java/cn/qiaqiatech/App.java
@@ -0,0 +1,44 @@
+package cn.qiaqiatech;
+
+import java.util.HashMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a1", "a1");
+        map.put("a2", "a2");
+        map.put("a3", "a3");
+        map.put("a4", "a4");
+        map.put("a5", "a5");
+        map.put("a6", "a6");
+        map.put("a7", "a7");
+        map.put("a8", "a8");
+        map.put("a9", "a9");
+        map.put("a10", "a10");
+        map.put("a11", "a11");
+        map.put("a12", "a12");
+        map.put("a13", "a13");
+        map.put("a14", "a14");
+        map.put("a15", "a15");
+        map.remove("a1");
+        map.remove("a2");
+        map.remove("a3");
+        map.remove("a4");
+        map.remove("a5");
+        map.remove("a6");
+        map.remove("a7");
+        map.remove("a8");
+        map.remove("a9");
+        map.remove("a10");
+        map.remove("a11");
+        map.remove("a12");
+        map.remove("a13");
+        map.remove("a14");
+        map.remove("a15");
+    }
+}
